0f049b2373c2a191055b4dbb10452954112cdf19
hyakt <hyakt0@gmail.com>
2021-04-06 22:00:00 +0900

    update: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
-import { useDeno } from "framework/react";
 import React from "react";
 import "https://esm.sh/tailwindcss/dist/tailwind.min.css";
 
 export default function Home() {
-  const version = useDeno(() => Deno.version.deno);
-
   return (
-    <div className="page">
-      <head>
-        <title>Hello World</title>
-      </head>
-      <h1 className='text-xl'>
-        hello alephjs
-      </h1>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center">
+        <div>
+          <img
+            className="rounded-full h-56 w-56"
+            src="https://www.gravatar.com/avatar/d9618150777ac86730044bb6de049016?s=240"
+          />
+        </div>
+        <div className="mt-8">
+          <div className="text-xl text-center font-semibold">Hayato Kajiyama</div>
+          <div className="text-base text-center text-gray-800 mt-2">
+            Web Engineer ♥ Emacs, JavaScript, HCI and 🍺
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
