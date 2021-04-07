0fcd83913054b8341061ecf05374247578b50b7a
hyakt <hyakt0@gmail.com>
2021-04-07 22:00:00 +0900

    update: scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,63 @@
-import React, { Children, useCallback, useRef, useState } from "react";
+import React, {
+  Children,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import "./index.css";
 
 const ScrollPageContainer: React.FC = ({ children }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const childrenCount = Children.count(children);
 
   const animationDelay = 800;
   const animationDelayBuffer = 200;
   const scrolling = useRef<boolean>(false);
+  const scrollContainer = useRef<any>(null);
+
+  const scrollDown = () => {
+    if (!scrolling.current && scrollContainer.current) {
+      if (currentPage > childrenCount - 2) return;
+
+      scrolling.current = true;
+
+      scrollContainer.current.style.transform =
+        `translateY(-${(currentPage + 1) * 100}vh)`;
+
+      setCurrentPage(currentPage + 1);
+
+      setTimeout(() => {
+        scrolling.current = false;
+      }, animationDelay + animationDelayBuffer);
+    }
+  };
+
+  const scrollUp = () => {
+    if (!scrolling.current && scrollContainer.current) {
+      if (currentPage === 0) return;
+      scrolling.current = true;
+
+      scrollContainer.current.style.transform =
+        `translateY(${(childrenCount - (currentPage + 2)) * 100}vh)`;
+
+      setCurrentPage(currentPage - 1);
+
+      setTimeout(() => {
+        scrolling.current = false;
+      }, animationDelay + animationDelayBuffer);
+    }
+  };
 
   const handleWheel = useCallback<React.WheelEventHandler>(
-    ({ currentTarget }) => {
-      if (!scrolling.current) {
-        scrolling.current = true;
-        currentPage === childrenCount - 1
-          ? setCurrentPage(0)
-          : setCurrentPage(currentPage + 1);
-        currentTarget.style.transform = `translateY(-${currentPage * 100}vh)`;
-
-        setTimeout(() => {
-          scrolling.current = false;
-        }, animationDelay + animationDelayBuffer);
+    (event) => {
+      if (Math.abs(event.deltaY) > 10) {
+        if (event.deltaY > 0) {
+          scrollDown();
+        } else {
+          scrollUp();
+        }
       }
     },
     [currentPage, childrenCount],
@@ -30,6 +66,7 @@ const ScrollPageContainer: React.FC = ({ children }) => {
   return (
     <div
       className="scroll-container"
+      ref={scrollContainer}
       onWheel={handleWheel}
       style={{ transition: `transform ${animationDelay}ms ease-in-out` }}
     >
@@ -74,6 +111,7 @@ export default function Home() {
       <Profile />
       <Skill />
       <Portfolio />
+      <Profile />
     </ScrollPageContainer>
   );
 }
