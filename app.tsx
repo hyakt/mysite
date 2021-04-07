13a5a482a01bf6f8b17829c526cfb17a4f7654fa
hyakt <hyakt0@gmail.com>
2021-04-07 22:00:00 +0900

    update: index

--- app.tsx
+++ app.tsx
@@ -1,4 +1,5 @@
 import React, { ComponentType } from "react";
+import "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css";
 
 export default function App(
   { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import "./index.css";
-import "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css";
 
 export default function Home() {
   return (
     <div className="container">
-      <div className="flex flex-col items-center">
-        <div>
-          <img
-            className="rounded-full h-56 w-56"
-            src="https://www.gravatar.com/avatar/d9618150777ac86730044bb6de049016?s=240"
-          />
+      <div className="profile">
+        <div className="profile__name">
+          Hayato Kajiyama
         </div>
-        <div className="mt-8">
-          <div className="text-xl text-center font-semibold">
-            Hayato Kajiyama
-          </div>
-          <div className="text-base text-center text-gray-800 mt-2">
-            Web Engineer ♥ Emacs, JavaScript, HCI and 🍺
+        <div className="profile-img">
+          <img className="profile-img__img" src="/profile.png" alt="profile" />
+        </div>
+        <div>
+          <div className="profile__subtitle">
+            Frontend-End Engineer
           </div>
         </div>
       </div>
