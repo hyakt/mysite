8d0d19096f471b4fdff5185e74381130985caa07
hyakt <hyakt0@gmail.com>
2021-04-08 22:00:00 +0900

    add: deploy command

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 deno install --unstable -A -f -n aleph https://deno.land/x/aleph/cli.ts
 deno install -qA -f -n vr https://deno.land/x/velociraptor/cli.ts
+npm i -g vercel
+npm i -g typescript
+npm i -g typescript-deno-plugin
