1cc867250f922757298cfa1a86d0e2ea038deccb
hyakt <hyakt0@gmail.com>
2021-04-14 22:00:00 +0900

    add: favicon

--- app.tsx
+++ app.tsx
@@ -5,11 +5,34 @@ export default function App(
   { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
 ) {
   return (
-    <main>
+    <>
       <head>
         <meta name="viewport" content="width=768" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#fefefe" />
+        <meta name="theme-color" content="#fefefe" />
       </head>
-      <Page {...pageProps} />
-    </main>
+      <main>
+        <Page {...pageProps} />
+      </main>
+    </>
   );
 }
