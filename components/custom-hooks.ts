a009607b749ce3c8ee72dca621de69f843d100d4
hyakt <hyakt0@gmail.com>
2021-04-27 22:00:00 +0900

    update: make hooks

---
+++ components/custom-hooks.ts
@@ -0,0 +1,21 @@
+import { useDeno } from "framework/react";
+import { safeLoadFront } from "https://esm.sh/yaml-front-matter@4.1.1";
+
+type MarkdownFile = {
+  filename: string;
+  readonly [key: string]: string;
+}
+
+const decoder = new TextDecoder();
+
+export const useMarkdownFiles = (path: string): MarkdownFile[] => (
+  useDeno(async () => {
+    const posts: MarkdownFile[] = [];
+    for await (const { name } of Deno.readDir(path)) {
+      const content = await Deno.readFile(`${path}/${name}`);
+      const { __content, ...meta } = safeLoadFront(decoder.decode(content));
+      posts.push({ filename: name.split(".")[0], ...meta })
+    }
+    return posts;
+  })
+)
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,26 +1,14 @@
 import React from "react";
-import { useDeno } from "framework/react";
-
-import { safeLoadFront } from "https://esm.sh/yaml-front-matter@4.1.1";
-
-const decoder = new TextDecoder();
+import { useMarkdownFiles } from "~/components/custom-hooks.ts"
 
 const POSTS_PATH = "./pages/posts";
 
 const Blog = () => {
-  const posts = useDeno(async () => {
-    const posts = [];
-    for await (const { name } of Deno.readDir(POSTS_PATH)) {
-      const content = await Deno.readFile(`${POSTS_PATH}/${name}`);
-      const { __content, ...meta } = safeLoadFront(decoder.decode(content));
-      posts.push({ ...meta, filename: name.split(".")[0] });
-    }
-    return posts;
-  });
+  const posts = useMarkdownFiles(POSTS_PATH)
 
   return (
     <>
-      {posts.map((e) => <div key={e}><a href={`/posts/${e.filename}`}>{e.filename}</a></div>)}
+      {posts.map((e) => <div key={e.filename}><a href={`/posts/${e.filename}`}>{e.filename}</a></div>)}
     </>
   );
 };
